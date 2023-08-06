ae184e284a666f1b62463bc98ecb0f793dc60787
Negrea-Andrei <negrea97@yahoo.com>
2023-08-06 13:53:17 +0300

    create a card where the weather details will de displayed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ async function testing() {
         const value = test.value
         const API = await fetch(`https://api.weatherapi.com/v1/current.json?key=95435ac00eca4f8d88a162138230108&q=${value}`);
         const realAPI = await API.json();
-        const bla = realAPI.current.temp_c
+        const bla = realAPI
         image.src = realAPI.current.condition.icon
         console.log(bla)
     }
