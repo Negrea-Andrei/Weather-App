53299e270ea378d1cf04e9c071edf5920e06badd
Negrea-Andrei <negrea97@yahoo.com>
2023-08-07 19:57:40 +0300

    add a message if the city is not found

--- src/index.js
+++ src/index.js
@@ -4,30 +4,54 @@ const press = document.querySelector('.press')
 const image = document.querySelector('.image')
 
 async function testing() {
+    const location = document.querySelector('.location');
+    const temperature = document.querySelector('.temperature');
+    const condition = document.querySelector('.condition');
+    const windSpeed = document.querySelector('.wind_speed');
+    const humidity = document.querySelector('.humidity')
+
     try {
-        const location = document.querySelector('.location');
-        const temperature = document.querySelector('.temperature');
-        const condition = document.querySelector('.condition');
-        const windSpeed = document.querySelector('.wind_speed');
-        const humidity = document.querySelector('.humidity')
 
         const cityName = city.value
         const countryName = country.value
 
-        const API = await fetch(`https://api.weatherapi.com/v1/current.json?key=95435ac00eca4f8d88a162138230108&q=${cityName}`);
-        const realAPI = await API.json();
-        const bla = realAPI
-        image.src = realAPI.current.condition.icon
-        console.log(bla)
+        if (countryName !== '') {
+            API = await fetch(`https://api.weatherapi.com/v1/current.json?key=95435ac00eca4f8d88a162138230108&q=${cityName},${countryName}`);
 
-        location.innerHTML = realAPI.location.name;
-        temperature.innerHTML =`Temperature: ${realAPI.current.temp_c}°C`;
-        condition.innerHTML = `Conditions: ${realAPI.current.condition.text}`;
-        windSpeed.innerHTML = `Wind Speed: ${realAPI.current.wind_kph} km/h`;
-        humidity.innerHTML = `Humidity: ${realAPI.current.humidity}`;
+            const realAPI = await API.json();
+            const bla = realAPI
+            image.src = realAPI.current.condition.icon
+            console.log(bla)
+
+            location.innerHTML = realAPI.location.name;
+            temperature.innerHTML = `Temperature: ${realAPI.current.temp_c}°C`;
+            condition.innerHTML = `Conditions: ${realAPI.current.condition.text}`;
+            windSpeed.innerHTML = `Wind Speed: ${realAPI.current.wind_kph} km/h`;
+            humidity.innerHTML = `Humidity: ${realAPI.current.humidity}`;
+        }
+        else {
+            API = await fetch(`https://api.weatherapi.com/v1/current.json?key=95435ac00eca4f8d88a162138230108&q=${cityName}`);
+
+            const realAPI = await API.json();
+            const bla = realAPI
+            image.src = realAPI.current.condition.icon
+            console.log(bla)
+
+            location.innerHTML = realAPI.location.name;
+            temperature.innerHTML = `Temperature: ${realAPI.current.temp_c}°C`;
+            condition.innerHTML = `Conditions: ${realAPI.current.condition.text}`;
+            windSpeed.innerHTML = `Wind Speed: ${realAPI.current.wind_kph} km/h`;
+            humidity.innerHTML = `Humidity: ${realAPI.current.humidity}`;
+        }
+
     }
     catch {
-        console.log("City not found")
+        location.innerHTML = "City not found";
+        temperature.innerHTML = '';
+        condition.innerHTML = '';
+        windSpeed.innerHTML = '';
+        humidity.innerHTML = '';
+        image.src = '../src/Profile1.jpg'
     }
 }
 
